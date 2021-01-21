ce429ab889a78b5a11fb97f1376209445f2d7872
unknown <kyleicban@yahoo.com>
2021-01-21 15:56:37 -0800

    different problem + aitken's algorithm

--- eulers.py
+++ eulers.py
@@ -1,7 +1,7 @@
 import math
 from matplotlib import pyplot as plt
 
-def eulers(f, t0, tn, y0, h):
+def eulers(f, t0, y0, N, h):
     i = 0
     ti = t0
     yi = y0
@@ -10,29 +10,48 @@ def eulers(f, t0, tn, y0, h):
     t = [ti]
     y = [yi]
 
-    print("Iteration: " + str(i))
-    print("t" + str(i) + " = " + str(ti))
-    print("y" + str(i) + " = " + str(yi))
+    # print("Iteration: " + str(i))
+    # print("t" + str(i) + " = " + str(ti))
+    # print("y" + str(i) + " = " + str(yi))
 
-    while(ti < tn):
+    while(i < N):
         yi_1 = yi + (h * f(ti, yi))
         yi = yi_1
         ti += h
         i += 1
 
-        print()
-        print("Iteration: " + str(i))
-        print("t" + str(i) + " = " + str(ti))
-        print("y" + str(i) + " = " + str(yi))
+        # print()
+        # print("Iteration: " + str(i))
+        # print("t" + str(i) + " = " + str(ti))
+        # print("y" + str(i) + " = " + str(yi))
 
         t.append(ti)
         y.append(yi)
 
+    return(yi)
     plt.scatter(t, y)
 
 def f(t, y):
-    return(-5 * y + 5 * t**2 + 2 * t)
+    return(4 * y * (1 - y))
 
-eulers(f, 0, 1, 1/3, 0.1)
+# eulers(f, 0, 1, 0.1, 0.003125)
 
-plt.show()
+# plt.show()
+
+def aitken(f, t0, y0, N, h):
+    yh = eulers(f, t0, y0, N, h)
+    yh_2 = eulers(f, t0, y0, N * 2, h / 2)
+    yh_4 = eulers(f, t0, y0, N * 4, h / 4)
+
+    frac = (yh - yh_2)/(yh_2 - yh_4)
+
+    p = math.log(frac)/math.log(2)
+    return(p)
+
+N = 10
+h = 0.1
+for i in range(4):
+    print("h = " + str(h))
+    print(aitken(f, 0, 0.1, N, h))
+    N = N * 2
+    h = h / 2
