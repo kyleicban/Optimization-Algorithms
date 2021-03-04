ff82a661ed3d2e284d022ab5a1dea578b9a8ddc4
unknown <kyleicban@yahoo.com>
2021-03-04 18:18:26 -0500

    TESTTTTTT

--- test.py
+++ test.py
@@ -2,3 +2,9 @@ import numpy as np
 
 A = np.array([[1, 0, 1], [0, 1, 0], [0, 0, 1]])
 print(np.linalg.solve(A, [1, 2, 3]))
+
+print(A)
+print(np.tril(A, -1))
+print(np.triu(A, 1))
+print(np.diag(np.diag(A)))
+print(np.linalg.inv(np.diag(np.diag(A))))
